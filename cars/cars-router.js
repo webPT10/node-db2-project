ec079cd7cb973bffccbf8a8819624b997629b053
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-28 16:30:06 -0800

    cars-router > set up basic structure

---
+++ cars/cars-router.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const db = require('../data/config');
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+
+})
+
+router.get(":id", async (req, res) => {
+
+})
+
+router.post('/', async (req, res) => {
+    
+})
