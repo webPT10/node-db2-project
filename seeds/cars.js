478b36405222242ec410597021929e5aab666a95
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-02 10:06:26 -0800

    seeds set up and adding to DB

--- seeds/cars.js
+++ seeds/cars.js
@@ -1,4 +1,25 @@
+exports.seed = async function(knex) {
+  // clears out all rows in table
+  await knex("cars").truncate();
 
-exports.seed = function(knex) {
-  
+  await knex("cars").insert([
+    {
+      vin: 252518,
+      make: "Mercedes-Benz",
+      model: "300D",
+      mileage: 303129
+    },
+    {
+      vin: 345789,
+      make: "Volkswagon",
+      model: "Golf",
+      mileage: 101025
+    },
+    {
+      vin: 448448,
+      make: "Volvo",
+      model: "V70",
+      mileage: 189585
+    }
+  ]);
 };
